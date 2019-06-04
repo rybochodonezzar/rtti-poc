896a7f7fe95f917e836ec35f69fd0c31a8133d32
Michał Swatowski <rybamorska@gmail.com>
2019-06-04 02:14:47 +0200

    finally finished

--- RTTIpoc.cpp
+++ RTTIpoc.cpp
@@ -11,7 +11,11 @@
 #include <typeindex>
 #include <stdexcept>
 #include <algorithm>
+#include <any>
+#include <utility>
 using namespace std;
+
+struct classname;
 
 /* generic functions */
 
@@ -56,9 +60,59 @@ auto runByBase = [](auto x, auto f)
 template <typename T> struct RTTIinfo;
 template <typename T> struct FieldTypeStuff; //need better name
 
+using AttrMap = unordered_map<type_index, any>;
+
 template <typename T> struct SimpleAttribute {
-    using storage_type = T;
+    using storage_type = vector<T>;
+    static void add(AttrMap& m, T x)
+    {
+        auto i = m.find(typeid(T));
+        if(i == m.end())
+            i = m.emplace(typeid(T), storage_type{}).first;
+        any_cast<storage_type>(&i->second)->emplace_back(x);
+    }
+    static const storage_type& get(const AttrMap& m)
+    {
+        return any_cast<const storage_type&>(m.at(typeid(T)));
+    }
 };
+
+template <typename T> struct DerivedAttribute {
+    using storage_type = vector<shared_ptr<T>>;
+    template <typename TT> static void add(AttrMap& m, TT x)
+    {
+        static_assert(is_base_of<T,TT>::value, "invalid type");
+        auto i = m.find(typeid(T));
+        if(i == m.end())
+            i = m.emplace(typeid(T), storage_type{}).first;
+        any_cast<storage_type>(&i->second)->emplace_back(shared_ptr<T>{
+            new TT{x},
+            [](T* p){ delete static_cast<TT*>(p); } //TODO: check if dynamic cast is not needed
+        });
+    }
+    static const storage_type& get(const AttrMap& m)
+    {
+        return any_cast<storage_type>(m.at(typeid(T)));
+    }
+};
+
+template <typename T> struct UniqueAttribute {
+    using storage_type = T;
+    static void add(AttrMap& m, T x)
+    {
+        if(!m.try_emplace(typeid(T), x).second)
+            throw runtime_error("inserting second UniqueAttribute");
+    }
+    static const storage_type& get(const AttrMap& m)
+    {
+        return any_cast<storage_type>(m.at(typeid(T)));
+    }
+};
+
+template <typename T> struct AttrType {
+    using type = SimpleAttribute<T>;
+};
+
 
 struct isField {
     const char* name;
@@ -67,13 +121,19 @@ struct isField {
     constexpr isField(const char* name, const char* typeName) : name{name}, typeName{typeName} {}
 };
 
+template <> struct AttrType<isField> { using type = DerivedAttribute<isField>; };
 
 template <typename T, typename C>
 struct Field : public isField {
     using type = T;
+    using attr_type = DerivedAttribute<isField>;
     T C::*fieldptr;
     constexpr Field(T C::*fieldptr, const char* name) : isField{name, FieldTypeStuff<T>::name}, fieldptr{fieldptr} {}
     string toString(void* source) { return FieldTypeStuff<T>::toString((C*)source->*fieldptr); }
+};
+
+template <typename T, typename C> struct AttrType<Field<T,C>> {
+    using type = DerivedAttribute<isField>;
 };
 
 template <> struct FieldTypeStuff<int> {
@@ -89,6 +149,7 @@ template <> struct FieldTypeStuff<string> {
 template <typename Type, typename Class> constexpr Field<Type, Class> field(Type Class::*ptr, const char* name) { return Field<Type, Class>{ptr, name}; }
 
 struct classname { const char* name; };
+template <> struct AttrType<classname> { using type = UniqueAttribute<classname>; };
 
 struct isDerived {};
 
@@ -152,6 +213,7 @@ struct TypeInfo {
     vector<type_index> baseIds;
     using deleterT = function<void(isField*)>;
     vector<unique_ptr<isField, deleterT>> fields;
+    AttrMap attrs;
     function<void*()> construct;
     function<void(void*)> serialize;
     TypeInfo() :
@@ -159,27 +221,45 @@ struct TypeInfo {
 	, id{typeid(void)}
 	, baseIds{}
 	, fields{}
+        , attrs{}
 	, construct{nullptr}
 	, serialize{nullptr}
 	{}
     template <typename T> void init()
     {
 	for_all(RTTIinfo<T>::info, [this](auto x){
-	    if constexpr (is_same<classname, decltype(x)>::value)
+            using xT = decltype(x);
+	    if constexpr (is_same<classname, xT>::value)
 		name = x.name;
-	    if constexpr (is_base_of<isDerived, decltype(x)>::value) {
-		using type = typename decltype(x)::type;
+	    if constexpr (is_base_of<isDerived, xT>::value) {
+		using type = typename xT::type;
 		baseIds.emplace_back(type_index{typeid(type)});
 	    }
 	    if constexpr (is_base_of<isField, deleterT>::value) {
-		using FieldT = decltype(x);
-                auto deleter = [](isField* p){ delete (static_cast<decltype(x)*>(p)); };
+		using FieldT = xT;
+                auto deleter = [](isField* p){ delete (static_cast<xT*>(p)); };
 		fields.emplace_back(unique_ptr<isField, deleterT>(new FieldT{x}, deleter));
 	    }
+            AttrType<xT>::type::add(attrs, x);
+
 	});
 	id = type_index{typeid(T)};
 	construct = [](){ return new T; };
 	serialize = [](void* p){ ::serialize<T>(*(T*)p); };
+    }
+    template <typename T> bool hasAttr()
+    {
+        return attrs.count(typeid(T));
+    }
+    bool hasAttr(type_index i)
+    {
+        return attrs.count(i);
+    }
+    template <typename T>
+    typename AttrType<T>::type::storage_type* get()
+    {
+        auto i = attrs.find(typeid(T));
+        return i != attrs.end() ? any_cast<typename AttrType<T>::type::storage_type>(&i->second) : nullptr;
     }
 };
 
@@ -256,22 +336,35 @@ struct B : virtual RTTIBase, A {
 template <> struct RTTIinfo<B> {
     constexpr static auto info = List(
 	classname{"B"},
-	derived<A>{},
+	//derived<A>{},
 	field(&B::intVal2, "intVal2")
+    );
+};
+
+struct C : virtual RTTIBase {
+    C() : RTTIBase{TypeManager::get().registerOrGetType<C>()} {}
+};
+template <> struct RTTIinfo<C> {
+    constexpr static auto info = List(
+	classname{"C"}
     );
 };
 
 int main()
 {
     auto& tm = TypeManager::get();
-    //tm.registerOrGetType<A>();
-    B b;
-    b.intVal = 7;
-    b.intVal2 = 666;
-    b.strVal = "asdasfasfdas";
-    serialize(b);
-    cout << b.isOfType<B>() << endl;
-    cout << b.isOfType<A>() << endl;
-    cout << b.isOfType<int>() << endl;
+    auto x = tm.registerOrGetType<C>().get<classname>();
+    cout << x->name << endl;
+    auto& y = tm.registerOrGetType<A>();
+   for(auto& z : *y.get<isField>())
+        cout << z->name << endl;
+    //B b;
+    //b.intVal = 7;
+    //b.intVal2 = 666;
+    //b.strVal = "asdasfasfdas";
+    //serialize(b);
+    //cout << b.isOfType<B>() << endl;
+    //cout << b.isOfType<A>() << endl;
+    //cout << b.isOfType<int>() << endl;
     return 0;
 }
